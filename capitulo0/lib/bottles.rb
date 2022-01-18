c651341c5a88db17541f2d8a2489a36f3d3d6e1f
anaplb <ana.paula@dcx.ufpb.br>
2022-01-18 18:08:40 -0300

    começando com código inicial

--- capitulo0/lib/bottles.rb
+++ capitulo0/lib/bottles.rb
@@ -1,22 +1,35 @@
 class Bottles
 
-    def verse(current_bottle)
-        if current_bottle == 0
-            return "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
-        elsif current_bottle == 1
-            return "#{current_bottle} bottle of beer on the wall, #{current_bottle} bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
-        else
-            last_verse = "#{current_bottle-1} bottles of beer on the wall."
-            if current_bottle == 2
-                last_verse = "#{current_bottle-1} bottle of beer on the wall."
-            end
-            return "#{current_bottle} bottles of beer on the wall, #{current_bottle} bottles of beer.\nTake one down and pass it around, #{last_verse}\n"
-        end
+    def song()
+        verses(99, 0)
     end
 
-    def verses(current_bottle, next_bottle)
-        for bottle in current_bottle.downto(next_bottle)
-            verse(bottle)
+    def verses(upper, lower)
+        upper.downto(lower).map { |i| verse(i)}.join("\n")
+    end 
+
+    def verse(number)
+        case number
+            when 0 
+                "No more bottles of beer on the wall, " +
+                "no more bottles of beer.\n" +
+                "Go to the store and buy some more, " + 
+                "99 bottles of beer on the wall.\n"
+            when 1
+                "1 bottle of beer on the wall, " +
+                "1 bottle of beer.\n" +
+                "Take it down and pass it around, " + 
+                "no more bottles of beer on the wall.\n"
+            when 2
+                "2 bottles of beer on the wall, " +
+                "2 bottles of beer.\n" +
+                "Take one down and pass it around, " + 
+                "1 bottle of beer on the wall.\n"
+            else
+                "#{number} bottles of beer on the wall, " +
+                "#{number} bottles of beer.\n" +
+                "Take one down and pass it around, " + 
+                "#{number-1} bottles of beer on the wall.\n"
         end
     end
 
